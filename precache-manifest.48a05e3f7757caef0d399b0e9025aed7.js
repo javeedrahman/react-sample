0418ef90e29d236e8295e781a1432b125d74dd39
javeed <javi.rahman@gmail.com>
2018-10-09 23:22:57 +0530

    Updates

---
+++ precache-manifest.48a05e3f7757caef0d399b0e9025aed7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84a7a7859984bb6fa551",
+    "url": "/react-sample/static/js/runtime~main.84a7a785.js"
+  },
+  {
+    "revision": "9aef450305ab64ff5725",
+    "url": "/react-sample/static/js/main.9aef4503.chunk.js"
+  },
+  {
+    "revision": "a0472c4d3f1d62ac328e",
+    "url": "/react-sample/static/js/1.a0472c4d.chunk.js"
+  },
+  {
+    "revision": "9aef450305ab64ff5725",
+    "url": "/react-sample/static/css/main.2d9a9605.chunk.css"
+  },
+  {
+    "revision": "8a73d5fe9012eef1d2098c1bba36b291",
+    "url": "/react-sample/index.html"
+  }
+];
